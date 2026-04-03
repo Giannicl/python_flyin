380b4911e0f03a26d9ded2db22b719edc09b20c6
Gianni Lieuw-A-Soe <glieuw-a@f1r1s17.codam.nl>
2026-04-03 18:39:35 +0200

    classes done - parser in progress

--- area.py
+++ area.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Dict, Optional
 
 
 class Zone:
@@ -21,17 +21,26 @@ class Zone:
 
 
 class Connection:
-    def __init__(self, zone1: Zone, zone2: Zone, max_links: int = 1) -> None:
+    def __init__(self, name: str, zone1: Zone, zone2: Zone, max_links: int = 1) -> None:
+        self.name: str = name
         self.zone1: Zone = zone1
         self.zone2: Zone = zone2
         self.max_link_capacity: int = max_links
 
 
-def main() -> None:
-    area = Zone("test", 2, 3)
-    print(area.current_drone)
-    print()
+class Graph:
+    def __init__(self) -> None:
+        self.zones: Dict[str, Zone] = {}
+        self.connections: Dict[str, Connection] = {}
 
+    def create_graph(self, element: Zone | Connection) -> None:
+        if isinstance(element, Zone):
+            self.zones[element.name] = element
+        elif isinstance(element, Connection):
+            self.connections[element.name] = element
 
-if __name__ == "__main__":
-    main()
+    def find_zone(self, zone_name: str) -> Zone | None:
+        return self.zones.get(zone_name)
+
+    def find_connection(self, connection_name: str) -> Connection | None:
+        return self.connections.get(connection_name)
--- file_parser.py
+++ file_parser.py
@@ -1,17 +1,19 @@
-from area import StartZone, EndZone, Connection, SpecialZone
+from area import Zone, Connection, Graph
 
 
-def parser(filename: str) -> list[StartZone | EndZone | Connection | SpecialZone]:
-    graph = []
+def parser(filename: str) -> list[Zone | Connection]:
+
     with open(filename, "r") as file:
         for line in file:
             line = line.strip()
             if not line or line.startswith("#"):
                 continue
-            if "hub" in line:
-                
+            if "start_hub" in line:
+                parts = split(lines, " ")
+                color = split(parts[3], "=")
+                start_zone = Zone("start", parts[2], parts[3], color[1])
 
-    return
+    return Graph
 
 
 if __name__ == "__main__":
